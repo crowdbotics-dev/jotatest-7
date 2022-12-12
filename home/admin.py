8d977f93bcd0e489a9b4f40a0febd8f10ae7f03c
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-12 21:25:46 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Casa
+admin.site.register(Casa)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Casa(models.Model):
+    'Generated Model'
+    space = models.BigIntegerField()
